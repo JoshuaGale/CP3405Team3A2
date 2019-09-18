1192d562ef600765af561b1cb3b71f1016eeab8d
thomasnapier <thomas.napier@my.jcu.edu.au>
2019-09-18 18:37:54 +1000

    Add login activity layout, Add default text styles for use throughout the app, Adjust manifest to lock orientation and set home screen

---
+++ app/src/main/java/com/example/cp3405_team_3_a2/LoginActivity.java
@@ -0,0 +1,23 @@
+package com.example.cp3405_team_3_a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void signInOnClick(View view){
+
+    }
+
+    public void createAccountOnClick(View view){
+
+    }
+}
