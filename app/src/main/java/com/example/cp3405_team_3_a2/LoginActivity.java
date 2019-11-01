988acc436669f6b1de71cd5b91d6ba42eebae2e9
jc463398 <james.raymer@my.jcu.edu.au>
2019-11-01 10:10:32 +1000

    Add details for jobs and recommendations

--- app/src/main/java/com/example/cp3405_team_3_a2/LoginActivity.java
+++ app/src/main/java/com/example/cp3405_team_3_a2/LoginActivity.java
@@ -23,43 +23,43 @@ public class LoginActivity extends AppCompatActivity {
         App.getContext().deleteDatabase("helper");
         databaseHelper = new DatabaseHelper(App.getContext());
         databaseHelper.insertStudent("joshua", "yeaboi",
-                0, "nothing", "zilch", "joshua gale", "nada", false, "nowhere",
+                0, "Bachelor of Information Technology", "James Cook University", "Joshua Gale", "Network Security", false, "Townsville",
                 "https://github.com/JoshuaGale", "https://www.linkedin.com/in/joshua-j-gale/");
         databaseHelper.insertStudent("thomas", "yeet",
-                0, "nothing", "zilch", "thomas napier", "something", true, "nowhere",
+                0, "nothing", "Bachelor of Information Technology", "thomas napier", "Data Analyst", true, "Brisbane",
                 "https://github.com/thomasnapier", "https://www.linkedin.com/in/thomas-napier-466b6a170/");
         databaseHelper.insertStaff("lindsay", "pog", 1, "IT Honours",
-                "Academic for 25 years", "Lindsay Ward", "Lecturer at JCU");
+                "James Cook University", "Lindsay Ward", "Lecturer at JCU");
         databaseHelper.insertCompany("google", "ezclap", 2,
-                "Google", "We are google, give us your data");
+                "Google", "Multinational, publicly-traded organization built around the company's hugely popular search engine.");
         long unixTime = System.currentTimeMillis() / 1000L;
-        databaseHelper.insertJob("google", "top meme", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme2", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme3", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme4", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme5", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme6", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme7", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme8", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme9", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme10", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme11", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
-        databaseHelper.insertJob("google", "top meme12", "you make memes", "computers", "100", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "DevOps Engineer", "A DevOps (development and operations) engineer is a specialised role that involves delivering a lot of code, quickly.", "computers", "$2,000,000.01", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Enterprise Architect", "An enterprise architect develops the plans and workflows for deploying and maintaining servers, software, and other IT assets. In other words, an enterprise architect is on the hook to come up with (or at least, contribute to) the IT strategy.", "computers", "$100,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Software Architect", "A software architect is responsible for setting the overall plan and strategy for executing on building software. You need to be technical for the role, but you’re really setting the pace and goals for other teams of developers", "computers", "$45,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Applications Architect", "The applications architect chooses the strategy for applications within a company: When to use existing tools, when to build new ones, and occasionally developing their own prototypes for further development.", "computers", "$87,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Data Warehouse Architect", "A data warehouse architect oversees all of a company’s generally-vast troves of data, and helps prepare it for analysis.", "computers", "$123,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Software Developer", "A Software developer will be responsible for developing and testing certain systems or applications within a company.", "computers", "$326,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Software Engineer", "A Software engineer will be tasked with developing, researching, and testing the company's software.", "computers", "$78,000.53", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Network Administrator", "A network administrator is the person designated in an organization whose responsibility includes maintaining computer infrastructures with emphasis on networking.", "computers", "$93,000.00", "10/11/10",unixTime, "sydney");
         databaseHelper.insertJob("google", "Graduate / Junior Developer", "Multiple entry-level positions are available to develop heterogeneous software for our award-winning enterprise storage technology. Talented graduates will be invited to join our core development team and engage in a rare opportunity to gain experience across a wide range of technologies.", "Full Time", "$80,000-100,000 p.a", "10/11/10",unixTime, "Brisbane Inner City");
-        databaseHelper.insertRecommendation("joshua", "Tom", 1, unixTime);
-        databaseHelper.insertRecommendation("joshua", "lindsay", 2, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom3", 3, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom4", 4, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom5", 5, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom6", 6, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom7", 7, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom8", 8, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom9", 9, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom10", 10, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom11", 11, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Tom12", 12, unixTime);
-        databaseHelper.insertRecommendation("joshua", "James", 0, unixTime);
-        databaseHelper.insertRecommendation("joshua", "Lindsay", 13, unixTime);
+        databaseHelper.insertJob("google", "DevOps Engineer", "A DevOps (development and operations) engineer is a specialised role that involves delivering a lot of code, quickly.", "computers", "$100,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Enterprise Architect", "An enterprise architect develops the plans and workflows for deploying and maintaining servers, software, and other IT assets. In other words, an enterprise architect is on the hook to come up with (or at least, contribute to) the IT strategy.", "computers", "$100,000.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Software Architect", "A software architect is responsible for setting the overall plan and strategy for executing on building software. You need to be technical for the role, but you’re really setting the pace and goals for other teams of developers", "computers", "$210.00", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertJob("google", "Applications Architect", "The applications architect chooses the strategy for applications within a company: When to use existing tools, when to build new ones, and occasionally developing their own prototypes for further development.", "$132,00.00", "100", "10/11/10",unixTime, "sydney");
+        databaseHelper.insertRecommendation("joshua", "Lindsay", 1, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Lindsay", 2, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Lindsay", 3, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Lindsay", 4, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Jason", 5, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Dianna", 6, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Jason", 7, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Dianna", 8, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Mangalem", 9, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Trina", 10, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Mangalem", 11, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Trina", 12, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Jason", 0, unixTime);
+        databaseHelper.insertRecommendation("joshua", "Jason", 13, unixTime);
 
         errorText = findViewById(R.id.error_text);
         emailText = findViewById(R.id.email_text_field);
